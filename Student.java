128da31e41b4b6b52ecd1f95c3f56ff711944018
l1lwave <135183716+l1lwave@users.noreply.github.com>
2024-11-03 00:44:50 +0200

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,5 +1,7 @@
 package lesson3firstex;
 
+import java.util.Objects;
+
 public class Student extends Human{
 	private int id;
 	private String groupName;
@@ -33,6 +35,25 @@ public class Student extends Human{
 	@Override
 	public String toString() {
 		return "Student [id=" + id + ", groupName=" + groupName + " " + super.toString() + "]";
-	}	
-	
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + Objects.hash(groupName, id);
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Student other = (Student) obj;
+		return Objects.equals(groupName, other.groupName) && id == other.id;
+	}		
 }
